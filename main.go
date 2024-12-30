d1904f10b8fb22e128f102a580d0994db2cbfbac
me-ron <meronkiflayyy@gmail.com>
2024-12-30 23:22:53 +0300

    bedesta

--- main.go
+++ main.go
@@ -8,37 +8,39 @@ import (
 	"quote-generator-backend/repositories"
 	"quote-generator-backend/routes"
 	"quote-generator-backend/services"
-
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-func Handler() *gin.Engine {
-    err := config.ConnectDB()
-    if err != nil {
-        log.Println(err.Error())
-    }
-    db := config.DB
-    quoteRepo := &repositories.QuoteRepository{Collection: db.Collection("quotes")}
-    userRepo := &repositories.UserRepository{Collection: db.Collection("users")}
+func Handler(w http.ResponseWriter, r *http.Request) {
+	// Initialize the database connection
+	err := config.ConnectDB()
+	if err != nil {
+		log.Println(err.Error())
+	}
 
-    quoteService := &services.QuoteService{Repo: quoteRepo}
-    userService := &services.UserService{Repo: userRepo}
+	db := config.DB
+	quoteRepo := &repositories.QuoteRepository{Collection: db.Collection("quotes")}
+	userRepo := &repositories.UserRepository{Collection: db.Collection("users")}
 
-    quoteController := &controllers.QuoteController{Service: quoteService}
-    userController := &controllers.UserController{Service: userService}
+	quoteService := &services.QuoteService{Repo: quoteRepo}
+	userService := &services.UserService{Repo: userRepo}
 
-    r := gin.Default()
-    routes.SetupRoutes(r, quoteController, userController)
+	quoteController := &controllers.QuoteController{Service: quoteService}
+	userController := &controllers.UserController{Service: userService}
 
-    return r
+	// Setup the Gin router
+	app := gin.Default()
+	routes.SetupRoutes(app, quoteController, userController)
+
+	// Start serving the HTTP request with the Gin engine
+	app.ServeHTTP(w, r)
 }
 
 func main() {
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8080"
-    }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-    r := Handler()
-    r.Run("0.0.0.0:" + port)
 }

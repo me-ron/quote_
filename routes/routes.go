c729365c73201922c2c158d30ab24d786969b819
me-ron <meronkiflayyy@gmail.com>
2024-12-30 23:47:50 +0300

    sera meselegn

--- routes/routes.go
+++ routes/routes.go
@@ -1,15 +1,17 @@
 package routes
 
 import (
-    "github.com/gin-gonic/gin"
-    "quote-generator-backend/controllers"
+	"quote-generator-backend/controllers"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(r *gin.Engine, qc *controllers.QuoteController, uc *controllers.UserController) {
-    r.POST("/quotes", qc.AddQuote)
-    r.GET("/quotes/:category", qc.GetQuotesByCategory)
-    r.GET("/quotes/random", qc.GetRandomQuotes)
-	r.POST("/login", uc.LoginOrCreateUser)
-    r.POST("/users/:user_id/favorites", uc.AddFavorite)
-    r.GET("/users/:user_id/favorites", uc.GetFavorites)
+    r.POST("/quotes", cors.Default(), qc.AddQuote)
+    r.GET("/quotes/:category", cors.Default(), qc.GetQuotesByCategory)
+    r.GET("/quotes/random", cors.Default(), qc.GetRandomQuotes)
+	r.POST("/login", cors.Default(), uc.LoginOrCreateUser)
+    r.POST("/users/:user_id/favorites", cors.Default(), uc.AddFavorite)
+    r.GET("/users/:user_id/favorites", cors.Default(), uc.GetFavorites)
 }
